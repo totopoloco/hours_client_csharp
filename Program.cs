6767b9740ba52355b0d562d632e6d7334874cccd
Marco Tulio Ávila Cerón <marcotavilac@pm.me>
2024-05-08 15:42:03 +0200

    none initial entry

---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+
+namespace HoursNameSpace
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            //Create a new instance of the Hours class
+            int start = 0;
+            int lunchStart = 0;
+            int minutesOfLunchBreak = 30;
+
+            string uri = "http://localhost:8384/ranges";
+            if (args.Length >= 3)
+            {
+                start = int.Parse(args[0]);
+                lunchStart = int.Parse(args[1]);
+                minutesOfLunchBreak = int.Parse(args[2]);
+                uri += $"WithStartLunchAndMinutesOfLunchBreak/{start}/{lunchStart}/{minutesOfLunchBreak}";
+            }
+            else if (args.Length >= 1)
+            {
+                minutesOfLunchBreak = int.Parse(args[0]);
+                uri += $"/{minutesOfLunchBreak}";
+            }
+            else
+            {
+                uri += "/30";
+            }
+
+            Console.WriteLine(uri);
+            var response = await new HttpClient().GetStringAsync(uri);
+            var json = JObject.Parse(response);
+
+            // Display the rangeDetails array in a table format with separate columns for start and end times
+            Console.WriteLine("{0,-10} {1,-10} {2,-10} {3,-10}", "start", "end", "duration", "durationInHours");
+            Console.WriteLine(new string('-', 40));
+            foreach (var rangeDetail in json["rangeDetails"])
+            {
+                Console.WriteLine("{0,-10} {1,-10} {2,-10} {3,-10}", rangeDetail["range"]["start"], rangeDetail["range"]["end"], rangeDetail["duration"], rangeDetail["durationInHours"]);
+            }
+            Console.WriteLine();
+
+            // Display the remaining data
+            Console.WriteLine("{0,-20} {1,-20} {2,-20}", "totalHours", "totalHoursInHHMM", "expectedLunchTimeInHHMM");
+            Console.WriteLine(new string('-', 60));
+            Console.WriteLine("{0,-20} {1,-20} {2,-20}", json["totalHours"], json["totalHoursInHHMM"], json["expectedLunchTimeInHHMM"]);
+        
+        }
+    
+    }
+}
